955d4c56f1e3deb70d52ad5670cd41256a2b4e6a
jmckenney <joshmckenney@gmail.com>
2019-08-28 11:39:03 -0700

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,30 @@
-import postcss from 'rollup-plugin-postcss';
+import cpy from 'rollup-plugin-cpy';
 import { createCompatibilityConfig } from '@open-wc/building-rollup';
 
-export default createCompatibilityConfig({
-    input: './index.html',
-    plugins: [
-        postcss({
-            plugins: []
-        })
-    ]
+const config = createCompatibilityConfig({
+  input: './index.html',
 });
+
+// if you use an array of configs, you don't need the copy task to be executed for both builds.
+// we can add the plugin only to the first rollup config:
+export default [
+  // add plugin to the first config
+  {
+    ...config[0],
+    plugins: [
+      ...config[0].plugins,
+      cpy({
+        // copy over all images files
+        files: ['**/*.css'],
+        dest: 'dist',
+        options: {
+          // parents makes sure to preserve the original folder structure
+          parents: true
+        }
+      }),
+    ],
+  },
+
+  // leave the second config untouched
+  config[1],
+];
